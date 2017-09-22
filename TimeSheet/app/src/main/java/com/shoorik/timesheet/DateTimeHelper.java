b554295a444162adec4105330d01b0659516284f
Shoo-Rik <fejhua@mail.ru>
2017-09-23 01:43:26 +0300

    [TS]: Implements time zone

---
+++ TimeSheet/app/src/main/java/com/shoorik/timesheet/DateTimeHelper.java
@@ -0,0 +1,51 @@
+package com.shoorik.timesheet;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public final class DateTimeHelper {
+
+    private String _timeZone;
+
+    public DateTimeHelper(String timeZone) {
+
+        if (timeZone == null) {
+            throw new NullPointerException("timeZone");
+        }
+        _timeZone = timeZone;
+    }
+
+    public String GetTimeZoneString() {
+
+        return _timeZone;
+    }
+
+    public String GetLocalDateTimeString(String formatString, Date date) {
+
+        SimpleDateFormat format = new SimpleDateFormat(formatString, Locale.US);
+        format.setTimeZone(TimeZone.getTimeZone(_timeZone));
+        return format.format(date);
+    }
+
+    public String GetCurrentWeekDay() {
+
+        return GetCurrentWeekDay(new Date());
+    }
+
+    public String GetCurrentDateString(Date date) {
+
+        return GetLocalDateTimeString("yyyy-MM-dd", date);
+    }
+
+    public String GetCurrentTimeString(Date date) {
+
+        return GetLocalDateTimeString("HH:mm", date);
+    }
+
+    public String GetCurrentWeekDay(Date date) {
+
+        return GetLocalDateTimeString("EEEE", date);
+    }
+}
--- timesheet/app/src/main/java/com/shoorik/timesheet/MainActivity.java
+++ timesheet/app/src/main/java/com/shoorik/timesheet/MainActivity.java
@@ -7,19 +7,19 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String PREFS_FILE = "TimeSheet";
-    private static final String PREFS_START_TIME = "Start";
-    private static final String PREF_END_TIME = "End";
 
     private SharedPreferences _settings;
     private SharedPreferences.Editor _preferenceEditor;
     private Map<String, WeekDayInfo> _info;
+    private DateTimeHelper _dateTimeHelper;
 
     public MainActivity() {
 
@@ -33,52 +33,117 @@ public class MainActivity extends AppCompatActivity {
         _info.put(WeekDayName.Sunday, new WeekDayInfo(WeekDayName.Sunday));
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        _settings = getSharedPreferences(PREFS_FILE, MODE_PRIVATE);
+        _dateTimeHelper = new DateTimeHelper(getString(R.string.timeZone));
+        _settings = getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE);
 
-        String date = DateTimeUtil.GetCurrentDateString();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeZone(TimeZone.getTimeZone(_dateTimeHelper.GetTimeZoneString()));
 
-        String startId = String.format("%s-%s", date, PREFS_START_TIME);
-        String endId = String.format("%s-%s", date, PREF_END_TIME);
+        String currentWeekDay = _dateTimeHelper.GetCurrentWeekDay(calendar.getTime());
+
+        int previousWeekDays = (calendar.get(Calendar.DAY_OF_WEEK) + 5) % 7; // hack
+
+        for (int i = 0; i <= previousWeekDays; ++i) {
+
+            InitializeDateTime(calendar.getTime());
+            calendar.add(Calendar.DAY_OF_YEAR, -1);
+        }
 
         TextView startView = (TextView) findViewById(R.id.startText);
-        String startString = _settings.getString(startId, null);
-        startView.setText(startString);
-
+        if (_info.get(currentWeekDay).StartTime != null) {
+            startView.setText(_dateTimeHelper.GetCurrentTimeString(_info.get(currentWeekDay).StartTime));
+        }
         TextView endView = (TextView) findViewById(R.id.endText);
-        String endString = _settings.getString(endId, null);
-        endView.setText(endString);
+        if (_info.get(currentWeekDay).EndTime != null) {
+            endView.setText(_dateTimeHelper.GetCurrentTimeString(_info.get(currentWeekDay).EndTime));
+        }
     }
 
-    @Override
-    protected void onPause() {
-
+    @Override protected void onPause() {
         super.onPause();
 
-        String date = DateTimeUtil.GetCurrentDateString();
-
-        String startId = String.format("%s-%s", date, PREFS_START_TIME);
-        String endId = String.format("%s-%s", date, PREF_END_TIME);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeZone(TimeZone.getTimeZone(_dateTimeHelper.GetTimeZoneString()));
 
         _preferenceEditor = _settings.edit();
 
-        TextView startView = (TextView) findViewById(R.id.startText);
-        String startString = startView.getText().toString();
-        _preferenceEditor.putString(startId, startView.getText().toString());
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        int previousWeekDays = (dayOfWeek + 5) % 7; // hack
 
-        TextView endView = (TextView) findViewById(R.id.endText);
-        String endString = endView.getText().toString();
-        _preferenceEditor.putString(endId, endView.getText().toString());
+        for (int i = 0; i <= previousWeekDays; ++i) {
+
+            StoreDateTime(calendar.getTime());
+            calendar.add(Calendar.DAY_OF_YEAR, -1);
+        }
 
         _preferenceEditor.commit();
     }
 
-    private int GetWeekDayViewId(String weekDay, boolean isStart) {
+    private void InitializeDateTime(Date date) {
+
+        String dateString = _dateTimeHelper.GetCurrentDateString(date);
+        String weekDayName = _dateTimeHelper.GetCurrentWeekDay(date);
+
+        String startId = String.format("%s-%s", dateString, getString(R.string.start));
+        String endId = String.format("%s-%s", dateString, getString(R.string.end));
+
+        long startTime = 0;
+        try {
+            startTime = _settings.getLong(startId, 0);
+        }
+        catch (ClassCastException ex) {
+        }
+        if (startTime > 0) {
+            _info.get(weekDayName).StartTime = new Date(startTime);
+        }
+
+        long endTime = 0;
+        try {
+            endTime = _settings.getLong(endId, 0);
+        }
+        catch (ClassCastException ex) {
+        }
+        if (endTime > 0) {
+            _info.get(weekDayName).EndTime = new Date(endTime);
+        }
+
+        String unknownTime = getString(R.string.unknownTime);
+
+        TextView startView = (TextView) findViewById(GetWeekDayViewId(weekDayName, true));
+        startView.setText(startTime > 0 ? _dateTimeHelper.GetCurrentTimeString(_info.get(weekDayName).StartTime) : unknownTime);
+
+        TextView endView = (TextView) findViewById(GetWeekDayViewId(weekDayName, false));
+        endView.setText(endTime > 0 ? _dateTimeHelper.GetCurrentTimeString(_info.get(weekDayName).EndTime) : unknownTime);
+
+        UpdateDuration(weekDayName);
+    }
+
+    private void StoreDateTime(Date date) {
+
+        String dateString = _dateTimeHelper.GetCurrentDateString(date);
+        String weekDayName = _dateTimeHelper.GetCurrentWeekDay(date);
+
+        WeekDayInfo dayInfo = _info.get(weekDayName);
+
+        if (dayInfo.StartTime != null) {
+
+            String startId = String.format("%s-%s", dateString, getString(R.string.start));
+            _preferenceEditor.putLong(startId, dayInfo.StartTime.getTime());
+        }
+
+        if (dayInfo.EndTime != null) {
+
+            String endId = String.format("%s-%s", dateString, getString(R.string.end));
+            _preferenceEditor.putLong(endId, dayInfo.EndTime.getTime());
+        }
+    }
+
+    private static int GetWeekDayViewId(String weekDay, boolean isStart) {
 
         switch (weekDay) {
             case WeekDayName.Monday:
@@ -102,7 +167,7 @@ public class MainActivity extends AppCompatActivity {
     public void onClickStartButton(View view) {
 
         Date now = new Date();
-        String day = DateTimeUtil.GetCurrentWeekDay(now);
+        String day = _dateTimeHelper.GetCurrentWeekDay(now);
         WeekDayInfo infoItem = _info.get(day);
         if (infoItem == null) {
             ShowMessage("Не удалось найти информацию о дне '" + day + "'");
@@ -111,9 +176,9 @@ public class MainActivity extends AppCompatActivity {
 
         infoItem.StartTime = now;
 
-        String currentTimeString = DateTimeUtil.GetCurrentTimeString(now);
+        String currentTimeString = _dateTimeHelper.GetCurrentTimeString(now);
         ((TextView)findViewById(R.id.startText)).setText(currentTimeString);
-        ((TextView)findViewById(GetWeekDayViewId(DateTimeUtil.GetCurrentWeekDay(), true))).setText(" " + currentTimeString + " ");
+        ((TextView)findViewById(GetWeekDayViewId(_dateTimeHelper.GetCurrentWeekDay(), true))).setText(currentTimeString);
 
         UpdateDuration(day);
     }
@@ -121,7 +186,7 @@ public class MainActivity extends AppCompatActivity {
     public void onClickEndButton(View view) {
 
         Date now = new Date();
-        String day = DateTimeUtil.GetCurrentWeekDay(now);
+        String day = _dateTimeHelper.GetCurrentWeekDay(now);
         WeekDayInfo infoItem = _info.get(day);
         if (infoItem == null) {
             ShowMessage("Не удалось найти информацию о дне '" + day + "'");
@@ -129,9 +194,9 @@ public class MainActivity extends AppCompatActivity {
         }
         infoItem.EndTime = now;
 
-        String currentTimeString = DateTimeUtil.GetCurrentTimeString(now);
+        String currentTimeString = _dateTimeHelper.GetCurrentTimeString(now);
         ((TextView)findViewById(R.id.endText)).setText(currentTimeString);
-        ((TextView)findViewById(GetWeekDayViewId(DateTimeUtil.GetCurrentWeekDay(), false))).setText(" " + currentTimeString + " ");
+        ((TextView)findViewById(GetWeekDayViewId(_dateTimeHelper.GetCurrentWeekDay(), false))).setText(currentTimeString);
 
         UpdateDuration(day);
     }
@@ -159,7 +224,7 @@ public class MainActivity extends AppCompatActivity {
             long hours = difference / 3600000;
             long minutes = (difference - hours) / 60000;
 
-            durationView.setText(String.format(" %1$02d:%2$02d ", hours, minutes));
+            durationView.setText(String.format("%1$02d:%2$02d", hours, minutes));
         }
     }
 
