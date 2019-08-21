d8bd10c9b39a9d1b49804b9f1f6f0bd13410b040
Shoo-Rik <fejhua@mail.ru>
2019-08-21 22:56:37 +0300

    [ANZA77]: Backup_20190810

---
+++ anza77.ru/backup_20190810/www/anza77.ru/administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-08-01 21:32:15 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-08-01T21:32:15+00:00	INFO 185.165.163.228	joomlafailure	Логин или пароль введены неправильно, либо такой учётной записи ещё не существует.
